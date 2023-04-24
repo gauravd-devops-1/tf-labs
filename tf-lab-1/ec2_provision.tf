0727f9c06e43f040d9ed732d94d4379aaa05b0c5
gauravd-n-able <108534734+gauravd-n-able@users.noreply.github.com>
2023-04-24 14:30:39 +0100

    Update ec2_provision.tf

--- tf-lab-1/ec2_provision.tf
+++ tf-lab-1/ec2_provision.tf
@@ -1,2 +1,15 @@
-/*
-*/
+provider "aws" {
+    region = "us-west-2"
+    access_key = "AKIAZYWPY7O3Z6PYCQNU"
+    secret_key = "BDoZsW1HAQRghl2EbJtC9rYztsmylUJbKCbalA8f"
+}
+
+
+
+resource "aws_instance" "example" {
+    ami = "ami-0b029b1931b347543"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "TF-Instance"
+    }
+}
